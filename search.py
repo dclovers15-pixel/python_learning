c5455297c47c55fcc2bb03a9a58d3763a64f8f85
2real <dclovers15@gmail.com>
2026-07-10 09:40:10 +0300

    ibrahim

---
+++ search.py
@@ -0,0 +1,19 @@
+import requests
+
+
+def main():
+    artwork = input("enter an artwork name: ")
+    artworks = get_artworks(query=artwork, limit=5)
+    for artwork in artworks:
+        print(artwork)
+
+
+def get_artworks(query, limit):
+    try:
+        response = requests.get(
+            "https://api.artic.edu/api/v1/artworks/search", {"q": query, "limit": limit}
+        )
+        content = response.json()
+        return [artwork["title"] for artwork in content["data"]]
+    except requests.HTTPError:
+        return []
